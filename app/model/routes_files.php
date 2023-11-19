7b6de9141ad6f76af2f72a4ef5a6cb30224b999f
Eri <103050349+Eriarer@users.noreply.github.com>
2023-11-18 22:49:45 -0600

    Definiendo rutas base

---
+++ app/model/routes_files.php
@@ -0,0 +1,12 @@
+<?php
+$CONFIG = array(
+  'base_url' => $_SERVER['DOCUMENT_ROOT'],
+  'P_css' => $CONFIG['base_url'] . '/view/css/',
+  'P_js' => $CONFIG['base_url'] . '/view/js/',
+  'P_lib' => $CONFIG['base_url'] . '/view/lib/',
+  'P_pages' => $CONFIG['base_url'] . '/view/html/',
+  'P_images' => $CONFIG['base_url'] . '/media/images/',
+  'P_products' => $CONFIG['base_url'] . '/media/products/',
+  'P_videos' => $CONFIG['base_url'] . '/media/multimedia/',
+  'P_music' => $CONFIG['base_url'] . '/media/multimedia/',
+);
