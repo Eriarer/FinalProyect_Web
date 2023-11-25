d305279c439f9d67f3bcc47ba3c352fd1c5be844
Eri <103050349+Eriarer@users.noreply.github.com>
2023-11-25 10:21:07 -0600

    Path Fixed

--- app/model/routes_files.php
+++ app/model/routes_files.php
@@ -1,12 +1,15 @@
 <?php
 $CONFIG = array(
-  'base_url' => $_SERVER['DOCUMENT_ROOT'],
-  'P_css' => $CONFIG['base_url'] . '/view/css/',
-  'P_js' => $CONFIG['base_url'] . '/view/js/',
-  'P_lib' => $CONFIG['base_url'] . '/view/lib/',
-  'P_pages' => $CONFIG['base_url'] . '/view/html/',
-  'P_images' => $CONFIG['base_url'] . '/media/images/',
-  'P_products' => $CONFIG['base_url'] . '/media/products/',
-  'P_videos' => $CONFIG['base_url'] . '/media/multimedia/',
-  'P_music' => $CONFIG['base_url'] . '/media/multimedia/',
+  'base_url' => $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/',
+  'P_app' => $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/app/',
+  'P_model' => $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/app/model/',
+  'P_view' => $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/app/view/',
+  'P_media' => $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/app/media/',
+  'P_fonts' => $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/app/media/fonts/',
+  'P_images' => $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/app/media/images/',
+  'P_multimedia' => $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/app/media/multimedia/',
+  'P_css' => $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/app/view/css/',
+  'P_php' => $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/app/view/html_php/',
+  'P_js' => $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/app/view/js/',
+  'P_lib' => $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/app/view/lib/'
 );
