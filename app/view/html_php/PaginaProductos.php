bddcef2d241f24fbfb6b187a5bba34ac333829aa
AketzaliCastorena <aketzalicastorena18@gmail.com>
2023-11-26 15:26:58 -0600

    borrador cartas producto

---
+++ app/view/html_php/PaginaProductos.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/Productoestilo.css">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Fila de Productos 1 -->
+    <div class="product-row">
+        <!-- Tarjeta de Producto 1 -->
+        <div class="product-card">
+            <figure class="img-product">
+                <img src="/../../media/images/productos/Perezoso.png" alt="Amor Perezoso">
+                <div class="whishlist"></div>
+            </figure>
+            <div class="info-product">
+                <h3 class="title-product">Peluche</h3>
+                <p class="desciption">Descripción del Peluche...</p>
+                <span class="price">$390</span>
+            </div>
+            <a href="#" class="btn-cart">Lo quiero!</a>
+        </div>
+
+        <!-- Tarjeta de Producto 2 -->
+        <div class="product-card">
+            <figure class="img-product">
+                <img src="imgenes/image (2).png" alt="Amor Perezoso">
+                <div class="whishlist"></div>
+            </figure>
+            <div class="info-product">
+                <h3 class="title-product">Peluche</h3>
+                <p class="desciption">Descripción del Peluche...</p>
+                <span class="price">$390</span>
+            </div>
+            <a href="#" class="btn-cart">Lo quiero!</a>
+        </div>
+    </div>
+
+    <!-- Fila de Productos 2 -->
+    <div class="product-row">
+        <!-- Tarjeta de Producto 3 -->
+        <div class="product-card">
+            <figure class="img-product">
+                <img src="imgenes/image (3).png" alt="Amor Perezoso">
+                <div class="whishlist"></div>
+            </figure>
+            <div class="info-product">
+                <h3 class="title-product">Peluche</h3>
+                <p class="desciption">Descripción del Peluche...</p>
+                <span class="price">$390</span>
+            </div>
+            <a href="#" class="btn-cart">Lo quiero!</a>
+        </div>
+
+        <!-- Tarjeta de Producto 4 -->
+        <div class="product-card">
+            <figure class="img-product">
+                <img src="imgenes/image (4).png" alt="Amor Perezoso">
+                <div class="whishlist"></div>
+            </figure>
+            <div class="info-product">
+                <h3 class="title-product">Peluche</h3>
+                <p class="desciption">Descripción del Peluche...</p>
+                <span class="price">$390</span>
+            </div>
+            <a href="#" class="btn-cart">Lo quiero!</a>
+        </div>
+    </div>
+
+    <!-- Fila de Productos 3 -->
+    <div class="product-row">
+        <!-- Tarjeta de Producto 5 -->
+        <div class="product-card">
+            <figure class="img-product">
+                <img src="imgenes/image (5).png" alt="Amor Perezoso">
+                <div class="whishlist"></div>
+            </figure>
+            <div class="info-product">
+                <h3 class="title-product">Peluche</h3>
+                <p class="desciption">Descripción del Peluche...</p>
+                <span class="price">$390</span>
+            </div>
+            <a href="#" class="btn-cart">Lo quiero!</a>
+        </div>
+
+        <!-- Tarjeta de Producto 6 -->
+        <div class="product-card">
+            <figure class="img-product">
+                <img src="imgenes/image (6).png" alt="Amor Perezoso">
+                <div class="whishlist"></div>
+            </figure>
+            <div class="info-product">
+                <h3 class="title-product">Peluche</h3>
+                <p class="desciption">Descripción del Peluche...</p>
+                <span class="price">$390</span>
+            </div>
+            <a href="#" class="btn-cart">Lo quiero!</a>
+        </div>
+    </div>
+
+    <!-- Fila de Productos 4 -->
+    <div class="product-row">
+        <!-- Tarjeta de Producto 7 -->
+        <div class="product-card">
+            <figure class="img-product">
+                <img src="imgenes/image (7).png" alt="Amor Perezoso">
+                <div class="whishlist"></div>
+            </figure>
+            <div class="info-product">
+                <h3 class="title-product">Peluche</h3>
+                <p class="desciption">Descripción del Peluche...</p>
+                <span class="price">$390</span>
+            </div>
+            <a href="#" class="btn-cart">Lo quiero!</a>
+        </div>
+
+        <!-- Tarjeta de Producto 8 -->
+        <div class="product-card">
+            <figure class="img-product">
+                <img src="imgenes/image (8).png" alt="Amor Perezoso">
+                <div class="whishlist"></div>
+            </figure>
+            <div class="info-product">
+                <h3 class="title-product">Peluche</h3>
+                <p class="desciption">Descripción del Peluche...</p>
+                <span class="price">$390</span>
+            </div>
+            <a href="#" class="btn-cart">Lo quiero!</a>
+        </div>
+    </div>
+</body>
+</html>
