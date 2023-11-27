a742363ebe1c531d7fd88d0253f8876ae0e3d6aa
Eri <103050349+Eriarer@users.noreply.github.com>
2023-11-27 13:12:34 -0600

    captcha color  and lines

--- app/model/captcha/captcha_generator.php
+++ app/model/captcha/captcha_generator.php
@@ -3,16 +3,29 @@ session_start();
 $FONTPATH = __DIR__ . '/../../media/fonts/COMICATE.TTF';
 $WIDT = 200;
 $HEIGHT = 50;
+$LINES = 3;
 
 $text = captchaText(6);
+// guardar el texto en la sesión
+$_SESSION['captcha'] = implode('', $text);
+// guardar la sesión para que se actualice el valor de $_SESSION['captcha']
+session_write_close();
 
 $image = imagecreatetruecolor($WIDT, $HEIGHT);
 // asignar un color de fondo
-$color = getRandColor($image, 200, 255);
+$color = getRandColor($image, 210, 255);
 // dibujar un rectángulo relleno
 imagefilledrectangle($image, 0, 0, $WIDT, $HEIGHT, $color);
 //agregarle el texto
-imagettftext($image, 20, getRandomAngle(), 50, 40, getRandColor($image, 0, 100), $FONTPATH, implode('', $text));
+for ($i = 0; $i < count($text); $i++) {
+  $color = getRandColor($image, 0, 100);
+  imagettftext($image, 30, getRandomAngle(), 15 + ($i * 30), 40, $color, $FONTPATH, $text[$i]);
+}
+// agregarle líneas de izquierda a derecha de la imagen
+for ($i = 0; $i < $LINES; $i++) {
+  $color = getRandColor($image, 0, 100);
+  imageline($image, 0, mt_rand(0, $HEIGHT), $WIDT, mt_rand(0, $HEIGHT), $color);
+}
 
 header('Content-type: image/jpeg');
 imagejpeg($image);
@@ -35,5 +48,5 @@ function getRandColor($image, $min, $max) {
 }
 
 function getRandomAngle() {
-  return mt_rand(-20, 20);
+  return mt_rand(-30, 30);
 }
