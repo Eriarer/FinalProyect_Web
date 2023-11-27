679511189c6e2e7ed42ecd90b35677378c0b8a1e
Eri <103050349+Eriarer@users.noreply.github.com>
2023-11-27 14:00:43 -0600

    captcha generator

--- app/model/captcha/captcha_generator.php
+++ app/model/captcha/captcha_generator.php
@@ -1,9 +1,15 @@
 <?php
 session_start();
 $FONTPATH = __DIR__ . '/../../media/fonts/COMICATE.TTF';
-$WIDT = 200;
+$TARGETDIR = __DIR__ . '/../../media/images/captcha/captcha.jpg';
+$WIDTH = 200;
 $HEIGHT = 50;
-$LINES = 3;
+$LINES = rand(5, 7);
+$ANGLE = 25;
+$FREQUENCY_X = 20;
+$AMPLITUDE_X = 1;
+$FREQUENCY_Y = 5;
+$AMPLITUDE_Y = 1;
 
 $text = captchaText(6);
 // guardar el texto en la sesión
@@ -11,32 +17,49 @@ $_SESSION['captcha'] = implode('', $text);
 // guardar la sesión para que se actualice el valor de $_SESSION['captcha']
 session_write_close();
 
-$image = imagecreatetruecolor($WIDT, $HEIGHT);
+$image = imagecreatetruecolor($WIDTH, $HEIGHT);
 // asignar un color de fondo
-$color = getRandColor($image, 210, 255);
+$color_component = getColorForImage($image, 210, 255);
 // dibujar un rectángulo relleno
-imagefilledrectangle($image, 0, 0, $WIDT, $HEIGHT, $color);
+imagefilledrectangle($image, 0, 0, $WIDTH, $HEIGHT, $color_component);
 //agregarle el texto
 for ($i = 0; $i < count($text); $i++) {
-  $color = getRandColor($image, 0, 100);
-  imagettftext($image, 30, getRandomAngle(), 15 + ($i * 30), 40, $color, $FONTPATH, $text[$i]);
+  $color_component = getColorForImage($image, 0, 100);
+  imagettftext($image, 30, getRandomAngle($ANGLE), 15 + ($i * 30), 40, $color_component, $FONTPATH, $text[$i]);
 }
-// agregar 'warps' a la imagen
-for ($i = 0; $i < 10; $i++) {
-  $color = getRandColor($image, 0, 100);
-  imagearc($image, mt_rand(0, $WIDT), mt_rand(0, $HEIGHT), mt_rand(0, $WIDT), mt_rand(0, $HEIGHT), mt_rand(0, 360), mt_rand(0, 360), $color);
+
+// agregar lineas a la imagen
+for ($i = 0; $i < $LINES; $i++) {
+  $color_component = getColorForImage($image, 0, 100);
+  imagearc($image, mt_rand(0, $WIDTH), mt_rand(0, $HEIGHT), mt_rand(0, $WIDTH), mt_rand(0, $HEIGHT), mt_rand(0, 360), mt_rand(0, 360), $color_component);
 }
+
+// agregar warped image
+for ($x = 0; $x < $WIDTH; $x++) {
+  for ($y = 0; $y < $HEIGHT; $y++) {
+    $index = imagecolorat($image, $x, $y);
+    $color_component = imagecolorsforindex($image, $index);
+
+    $color = imagecolorallocate($image, $color_component['red'], $color_component['green'], $color_component['blue']);
+
+    $imageX = $x + sin($y / $FREQUENCY_X) * $AMPLITUDE_X;
+    $imageY = $y + sin($x / $FREQUENCY_Y) * $AMPLITUDE_Y;
+    imagesetpixel($image, $imageX, $imageY, $color);
+  }
+}
+
 
 // Agregarle blur
 imagefilter($image, IMG_FILTER_GAUSSIAN_BLUR);
 
-header('Content-type: image/jpeg');
+// guardar la imagen
+imagejpeg($image, $TARGETDIR, 100);
 imagejpeg($image);
 imagedestroy($image);
 
 
 function captchaText($length) {
-  $pattern = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  $pattern = '123456789abcdefghijklmnopqrstuvwxzABCDEFGHIJKLMNOPQRSTUVWXZ';
   $max = strlen($pattern) - 1;
   $captcha = [];
   for ($i = 0; $i < $length; $i++) {
@@ -45,11 +68,11 @@ function captchaText($length) {
   return $captcha;
 }
 
-function getRandColor($image, $min, $max) {
+function getColorForImage($image, $min, $max) {
   // crear el objeto de color
   return imagecolorallocate($image, mt_rand($min, $max), mt_rand($min, $max), mt_rand($min, $max));
 }
 
-function getRandomAngle() {
-  return mt_rand(-30, 30);
+function getRandomAngle($angle) {
+  return mt_rand(-$angle, $angle);
 }
