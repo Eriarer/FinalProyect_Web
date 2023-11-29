8a72199be7cf9e8bf1cc53e95d3fb926c1ad66ad
Eri <103050349+Eriarer@users.noreply.github.com>
2023-11-28 22:23:47 -0600

    added functionality to update IRT the table

--- app/view/html_php/BajasProd.php
+++ app/view/html_php/BajasProd.php
@@ -41,12 +41,15 @@
   </div>
   <?php require_once 'footer.php'; ?>
   <script>
+    var old_productos = null;
     $(document).ready(function() {
-
       resizeTable();
+
       $(window).on("resize", function() {
         resizeTable();
       });
+
+      updateTable();
     });
 
     /**
@@ -75,33 +78,73 @@
         cancelButtonText: "Cancelar"
       }).then((result) => {
         if (result.isConfirmed) {
-          // Realiza la solicitud AJAX para eliminar el producto
-          $.ajax({
-            type: "POST",
-            url: "eliminar_producto.php",
-            data: {
-              id: id
-            },
-            success: function(response) {
-              if (response == "success") {
-                Swal.fire({
-                  title: "Producto eliminado",
-                  text: "El artículo ha sido eliminado correctamente",
-                  icon: "success"
-                }).then((result) => {
-                  location.reload();
-                });
-              } else {
-                Swal.fire({
-                  title: "Error",
-                  text: "Ha ocurrido un error al eliminar el producto",
-                  icon: "error"
-                });
-              }
-            }
-          });
+    function updateTable() {
+      // Uso de la función con un callback
+      obtenerTablaProductos(function(productos) {
+        if (!isEqual(old_productos, productos)) {
+          console.log("Actualizando tabla");
+          old_productos = productos;
+          rellenarTabla(productos);
         }
       });
+      setTimeout(updateTable, 500);
+    }
+
+    function isEqual(a, b) {
+      // Función para comparar dos objetos
+      function isEqual(obj1, obj2) {
+        return JSON.stringify(obj1) === JSON.stringify(obj2);
+      }
+    }
+
+    function obtenerTablaProductos(callback) {
+      $.ajax({
+        type: "POST",
+        url: "../../model/DB/manejoProductos.php",
+        data: {
+          method: "getAllProducts"
+        },
+        success: function(response) {
+          // Eliminar cualquier texto al inicio que diga 'string(*) '
+          var productos = JSON.parse(response);
+          callback(productos);
+        },
+        error: function(xhr, status, error) {
+          console.error("Error en la solicitud AJAX:", status, error);
+          callback(null);
+        }
+      });
+    }
+
+    function rellenarTabla(productos) {
+      if (productos == null) {
+        return;
+      }
+      // crear una fila por cada producto con sus respectivos datos
+      var tbody = $("tbody");
+      tbody.empty();
+      for (var i = 0; i < productos.length; i++) {
+        var producto = productos[i];
+        var tr = $("<tr></tr>");
+        tr.addClass("text-center");
+        tr.append("<th>" + producto.prod_id + "</th>");
+        tr.append("<td>" + producto.categoria + "</td>");
+        tr.append("<td>" + producto.prod_name + "</td>");
+        tr.append("<td>" + producto.prod_description + "</td>");
+        // la ruta de la imagen se guarda en la base de datos va a ser local, por lo que se debe agregar el path de la carpeta
+        // cortar la ruta de la imagen '/' y obtener el ultimo elemento
+        console.log(producto.prod_imgPath);
+        var prodImg = producto.prod_imgPath.split("/");
+        prodImg = prodImg[prodImg.length - 1];
+        var url = "../../media/images/productos/" + prodImg;
+        console.log(url);
+        tr.append("<td><img src='" + url + "' alt='" + prodImg + "' width='100px'></td>");
+        tr.append("<td>" + producto.prod_stock + "</td>");
+        tr.append("<td>" + producto.prod_precio + "</td>");
+        tr.append("<td>" + producto.prod_descuento + "</td>");
+        tr.append("<td><button class='btn btn-danger' onclick='confirmarEliminar(" + producto.prod_id + ")'>Eliminar</button></td>");
+        tbody.append(tr);
+      }
     }
   </script>
 </body>
