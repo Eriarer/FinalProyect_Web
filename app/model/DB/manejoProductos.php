c535d39fb23f30dd79eebedeaf72bf5c4187a7e6
mariana-avila-rivera <marianaavilarivera@gmail.com>
2023-11-29 22:37:48 -0600

    Arch para mandar llamar a las funciones de Prod.

--- app/model/DB/manejoProductos.php
+++ app/model/DB/manejoProductos.php
@@ -17,6 +17,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $json =  $db->getAllProducts();
     echo $json;
     return $json;
+  } else if ($method === "getProduct") {
+    echo "getProduct " . $_POST['id'];
+  } else if ($method === "modifyProduct") {
+    $id = $_POST['id'];
+    $categoria = $_POST['categoria'];
+    $nombre = $_POST['nombre'];
+    $descripcion = $_POST['descripcion'];
+    $stock = $_POST['stock'];
+    $precio = $_POST['precio'];
+    $descuento = $_POST['descuento'];
+    $imagen = $_FILES["imagen"]["name"];
+    //$id, $categoria, $prod_name, $prod_description, $prod_imgPath, $prod_stock, $prod_precio, $prod_descuento
+    $response = $db->modifyProduct($id, $categoria, $nombre, $descripcion, $imagen, $stock, $precio, $descuento);
+    echo $response ? "success" : "error";
+    return $response ? "success" : "error";
   }
   echo "error";
   return "error";
