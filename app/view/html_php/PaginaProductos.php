2d7fbe8b10eac53a009f871a767e950fb5449d43
mariana-avila-rivera <marianaavilarivera@gmail.com>
2023-12-01 00:39:22 -0600

    tienda enlazada con la BD y filtrado por categoria

--- app/view/html_php/PaginaProductos.php
+++ app/view/html_php/PaginaProductos.php
@@ -1,162 +1,3 @@
-<?php
-include_once __DIR__ . '/../../../app/model/routes_files.php';
-$rutaBase = __DIR__ . '/../../media/images/productos/';
-
-$productos = array(
-  'Por Ocasiones' => array(
-    array(
-      'nombre' => 'Amor Perezoso',
-      'descripcion' => 'Este peluche es un encantador perezoso, diseñado para irradiar amor y ternura. Su pelaje es largo y sedoso, invitando a ser acariciado, en tonos de gris suave que recuerdan a la lujosa textura de un abrigo de invierno.',
-      'precio' => 390,
-      'imagen' => '../../media/images/productos/Perezoso.png',
-      'descuento' => 10,
-      'existencias' => 100,
-      'categoria' => 'ocasiones'
-    ),
-    array(
-      'nombre' => 'Valentina Elefantina',
-      'descripcion' => 'Elaborado con materiales hipoalergénicos y suaves al tacto, este peluche es perfecto para todas las edades. Su relleno es esponjoso, lo que lo hace extremadamente abrazable y reconfortante.',
-      'imagen' => '../../media/images/productos/Elefante.png',
-      'precio' => 190,
-      'descuento' => 20,
-      'existencias' =>
-      10,
-      'categoria' => 'ocasiones'
-    ),
-    array(
-      'nombre' => 'Jorge Graduado',
-      'descripcion' => 'Con un tamaño mediano, este peluche es ideal tanto para decorar un espacio como para ser un compañero de abrazos.',
-      'precio' => 150,
-      'imagen' => '../../media/images/productos/chango.png',
-      'descuento' => 10,
-      'existencias' =>
-      0,
-      'categoria' => 'ocasiones'
-    ),
-    array(
-      'nombre' => 'Pablo Graduado',
-      'descripcion' => 'Elaborado con materiales hipoalergénicos y suaves al tacto, este peluche es perfecto para todas las edades. Su relleno es esponjoso, lo que lo hace extremadamente abrazable y reconfortante.',
-      'precio' => 290,
-      'imagen' => '../../media/images/productos/pingui.png',
-      'descuento' => 10,
-      'existencias' =>
-      250,
-      'categoria' => 'ocasiones'
-    ),
-    array(
-      'nombre' => 'Picasso Navideño',
-      'descripcion' => 'Este peluche es un encantador pingüino, diseñado para irradiar amor y ternura. Invitando a ser acariciado, en tonos navideños suave que recuerdan a la lujosa textura de un abrigo de invierno.',
-      'precio' => 270,
-      'imagen' => '../../media/images/productos/pinguiNav.png',
-      'descuento' => 10,
-      'existencias' => 0,
-      'categoria' => 'ocasiones'
-    ),
-    array(
-      'nombre' => 'Copito',
-      'descripcion' => 'Elaborado con materiales hipoalergénicos y suaves al tacto, este peluche es perfecto para todas las edades. Su relleno es esponjoso, lo que lo hace extremadamente abrazable y reconfortante.',
-      'precio' => 190,
-      'imagen' => '../../media/images/productos/monoNiev.png',
-      'descuento' => 10,
-      'existencias' => 20,
-      'categoria' => 'ocasiones'
-    ),
-    array(
-      'nombre' => 'Salem',
-      'descripcion' => 'Este peluche de gatito es más que un juguete; es un compañero que lleva un mensaje de amor y tranquilidad, perfecto para regalar en ocasiones especiales como halloween, dia de muertos o simplemente como un gesto para expresar cariño y afecto.',
-      'precio' => 100,
-      'imagen' => '../../media/images/productos/gato.png',
-      'descuento' => 10,
-      'existencias' => 0,
-      'categoria' => 'ocasiones'
-    ),
-    array(
-      'nombre' => 'Xochitl',
-      'descripcion' => 'Elaborado con materiales hipoalergénicos y suaves al tacto, este peluche es perfecto para todas las edades. Su relleno es esponjoso, lo que lo hace extremadamente abrazable y reconfortante.',
-      'precio' => 250,
-      'imagen' => '../../media/images/productos/florC.png',
-      'descuento' => 10,
-      'existencias' => 0,
-      'categoria' => 'ocasiones'
-    )
-  ),
-  'Especiales' => array(
-    array(
-      'nombre' => 'Party Rex',
-      'descripcion' => 'Este peluche es un encantador de dinosaurio, diseñado para irradiar amor y ternura. Su pelaje es sedoso, invitando a ser acariciado, en tonos de verdes suave .',
-      'precio' => 300,
-      'imagen' => '../../media/images/productos/DinoPumpe.png',
-      'descuento' => 10,
-      'existencias' => 0,
-    ),
-    array(
-      'nombre' => 'Bob Stegosaurus',
-      'descripcion' => 'Elaborado con materiales hipoalergénicos y suaves al tacto, este peluche es perfecto para todas las edades. Su relleno es esponjoso, lo que lo hace extremadamente abrazable y reconfortante.',
-      'precio' => 350,
-      'imagen' => '../../media/images/productos/DinoGlobos.png',
-      'descuento' => 10,
-      'existencias' => 76,
-    ),
-    array(
-      'nombre' => 'Fénix',
-      'descripcion' => 'Elaborado con materiales hipoalergénicos y suaves al tacto, este peluche es perfecto para todas las edades.',
-      'precio' => 450,
-      'imagen' => '../../media/images/productos/DragoNar.png',
-      'descuento' => 10,
-      'existencias' => 0,
-    ),
-    array(
-      'nombre' => 'Akura',
-      'descripcion' => 'Con un tamaño mediano, este peluche es ideal tanto para decorar un espacio como para ser un compañero de abrazos',
-      'precio' => 400,
-      'imagen' => '../../media/images/productos/DragoVer.png',
-      'descuento' => 10,
-      'existencias' => 10,
-    ),
-    array(
-      'nombre' => 'Michi Melody',
-      'descripcion' => 'Este peluche es un encantador michi, diseñado para irradiar amor y ternura. Su pelaje es suave, invitando a ser acariciado, en tonos de gris suave que recuerdan a la lujosa textura de un abrigo de invierno.',
-      'precio' => 300,
-      'imagen' => '../../media/images/productos/Melody.png',
-      'descuento' => 10,
-      'existencias' => 0,
-    ),
-    array(
-      'nombre' => 'Cinnamoroll',
-      'descripcion' => 'Elaborado con materiales hipoalergénicos y suaves al tacto, este peluche es perfecto para todas las edades.',
-      'precio' => 370,
-      'imagen' => '../../media/images/productos/Cinam.png',
-      'descuento' => 10,
-      'existencias' => 10,
-    ),
-    array(
-      'nombre' => 'Ake Pirata',
-      'descripcion' => 'Es el compañero perfecto para los jóvenes aventureros y los entusiastas de las historias de piratas. Es un regalo ideal que inspira imaginación y lleva la diversión de los cuentos de piratas al cuarto de juegos.',
-      'precio' => 850,
-      'imagen' => '../../media/images/productos/Pirata.png',
-      'descuento' => 10,
-      'existencias' => 0,
-    ),
-    array(
-      'nombre' => 'Super Pau',
-      'descripcion' => 'Este Super Panda Héroe no es solo un peluche, es un símbolo de coraje y bondad, ofreciendo horas de juego imaginativo y convirtiéndose en un confidente para los pequeños héroes en entrenamiento.',
-      'precio' => 280,
-      'imagen' => '../../media/images/productos/Super.png',
-      'descuento' => 10,
-      'existencias' => 10,
-    ),
-    array(
-      'nombre' => 'Ajoudini',
-      'descripcion' => 'Elaborado con materiales hipoalergénicos y suaves al tacto, este peluche es perfecto para todas las edades.',
-      'precio' => 300,
-      'imagen' => '../../media/images/productos/Mago.png',
-      'descuento' => 10,
-      'existencias' => 0,
-    )
-  )
-);
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -165,7 +6,7 @@ $productos = array(
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../css/Productoestilo.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-  <title>Document</title>
+  <title>Tienda</title>
   <!-- Boostrap v4.6.% -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous" />
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
@@ -175,47 +16,107 @@ $productos = array(
   <!-- CSS -->
   <link rel="stylesheet" href="../css/main.css">
 </head>
-<?php include_once("navbar.php") ?>
 
 <body>
-  <div class="container-fluid">
-    <?php
-    foreach ($productos as $categoria => $productosCategoria) {
-      echo "<h1 class='text-center'>$categoria</h1>";
-      echo "<hr>";
-      echo "<div class='row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4'>";
-      foreach ($productosCategoria as $producto) {
-        echo "<div class='col p-2'>";
-        echo "  <div class='myCard'>";
-        echo "      <div class='cardHeader'>";
-        echo "      <img src='" . $producto['imagen'] . "' alt='" . $producto['nombre'] . "'>";
-        echo "      <div class='overlay'>$" . $producto['precio'] . "</div>";
-        echo "      <div class='descuento'>";
-        if ($producto['descuento'] != 0) {
-          echo "Ahorra " . $producto['descuento'] . "%";
-        }
-        echo "      </div>";
-        echo "    </div>";
-        echo "    <div class='cardBody'>";
-        echo "      <h3 class='title-product'>" . $producto['nombre'] . "</h3>";
-        echo "      <p class='desciption'>" . $producto['descripcion'] . "</p>";
-        echo "      <span class='price'>Existencias: " . $producto['existencias'] . "</span>";
-        echo "    </div>";
-        echo "    <div class='cardFooter'>";
-        echo "      <a href='#' class='btn-cart'>Comprar ahora</a>";
-        echo "      <a href='#'><i class='nf nf-md-cart_plus'></i></a>";
-        echo "    </div>";
-        echo "  </div>";
-        echo "</div>";
-      }
-      echo "</div>";
+  <?php
+  include_once __DIR__ . '/../../model/DB/dataBaseCredentials.php';
+  include_once __DIR__ . '/../../model/routes_files.php';
+  include_once __DIR__ . '/../../model/DB/controllDB.php';
+
+  include_once("navbar.php");
+
+  $db = new dataBase($credentials, $CONFIG);
+  $productos = json_decode($db->getAllProducts(), true);
+  $categorias = [];
+  foreach ($productos as $producto) {
+    if (!in_array($producto['categoria'], $categorias)) {
+      array_push($categorias, $producto['categoria']);
     }
-    ?>
+  }
+  // crear un vector de productos, agrupados por categoria
+  $productosCategoria = array();
+  foreach ($categorias as $categoria) {
+    $productosCategoria[$categoria] = array();
+    foreach ($productos as $producto) {
+      if ($producto['categoria'] == $categoria) {
+        array_push($productosCategoria[$categoria], $producto);
+      }
+    }
+  }
+  ?>
+  <div class="container-fluid">
+    <div class="mt-4 col-3 ">
+      <label for="listCat">Categoria:</label>
+      <select class="form-control" id="listCat" name="listCat">
+        <optgroup class="groupCat">
+          <option value="0" selected="selected">Sin filtro</option>
+          <option value="1">Por ocasiones</option>
+          <option value="2">Especiales</option>
+        </optgroup>
+      </select>
+    </div>
+
+    <?php foreach ($categorias as $categoria) : ?>
+      <div id="<?= str_replace(" ", "_", $categoria) ?>">
+        <h1 class='text-center mt-4'><?= $categoria ?></h1>
+        <hr>
+        <div class='row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4'>
+          <?php foreach ($productosCategoria[$categoria] as $producto) :
+            $imagenBase = '../../media/images/productos/';
+            // quitarle a la ruta de la imagen
+            $imageName = explode("/", $producto['prod_imgPath'])[count(explode("/", $producto['prod_imgPath'])) - 1];
+            $targetImage = $imagenBase . $imageName;
+          ?>
+            <div class='col p-2'>
+              <div class='myCard'>
+                <div class='cardHeader'>
+                  <img src="<?= $targetImage ?>" alt="<?= $producto['prod_name'] ?>">
+                </div>
+                <div class='overlay'>$<?= $producto['prod_precio'] ?></div>
+
+                <?php if ($producto['prod_descuento'] != 0) : ?>
+                  <div class='descuento'>
+                    Ahorra <?= $producto['prod_descuento'] ?>%
+                  </div>
+                <?php endif; ?>
+
+
+                <div class='cardBody'>
+                  <h3 class='title-product'><?= $producto['prod_name'] ?></h3>
+                  <p class='desciption'><?= $producto['prod_description'] ?></p>
+                  <span class='price'>Existencias: <?= $producto['prod_stock'] ?></span>
+                </div>
+                <div class='cardFooter'>
+                  <a href='#' class='btn-cart'>Comprar ahora</a>
+                  <a href='#'><i class='nf nf-md-cart_plus'></i></a>
+                </div>
+              </div>
+            </div>
+          <?php endforeach; ?>
+        </div>
+      </div>
+    <?php endforeach; ?>
+
   </div>
+
   <?php include_once("footer.php") ?>
   <script>
     $(document).ready(function() {
-      $('.myCard').addClass("animate__animated animate__fadeInUp");
+      $('.myCard').addClass(" animate__animated animate__fadeInUp");
+
+      $('#listCat').change(function() {
+        var cat = $(this).val();
+        if (cat == 0) {
+          $("#Por_ocasiones").show("slow");
+          $("#Ediciones_Especiales").show("slow");
+        } else if (cat == 1) {
+          $("#Ediciones_Especiales").hide("slow");
+          $("#Por_ocasiones").show("slow");
+        } else if (cat == 2) {
+          $("#Por_ocasiones").hide("slow");
+          $("#Ediciones_Especiales").show("slow");
+        }
+      });
     });
   </script>
 </body>
