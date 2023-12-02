41da01eeff7f7e5bb6c8c0d07379a32e4fdfd6c5
Eri <103050349+Eriarer@users.noreply.github.com>
2023-12-02 12:59:06 -0600

    fixed  paths and includes css

--- app/view/html_php/navbar.php
+++ app/view/html_php/navbar.php
@@ -1,16 +1,19 @@
-<link rel="stylesheet" href="../css/headers/navbar.css">
-<link rel="shortcut icon" href="../../media/imagenes/oso-de-peluche.png" type="image/x-icon">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-<!-- CSS -->
-<link rel="stylesheet" href="../css/main.css">
+<!-- Jquery -->
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
 
 <?php
 require_once __DIR__ . '/../../model/routes_files.php';
 $image = $CONFIG['P_images'] . 'LogoSF.png';
 $base = $CONFIG['base_url'];
 $php = $CONFIG['P_php'];
+$css = $CONFIG['P_view'] . 'css/';
+$navbarCSS = $css . 'headers/navbar.css';
 ?>
+
+<link rel="stylesheet" href="<?= $navbarCSS ?>">
 <nav class="navbar navbar-expand-sm navbar-dark" id="barramenu">
     <a class="navbar-brand" href="#">
         <img src="<?= $image ?>" width="100" height="auto">
@@ -21,19 +24,19 @@ $php = $CONFIG['P_php'];
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto">
             <li class="nav-item" tootlip="Inicio">
-                <a class="nav-link" href="<?= $base . 'index.php'?>"><i class="nf nf-md-home"></i></a>
+                <a class="nav-link" href="<?= $base . 'index.php' ?>"><i class="nf nf-md-home"></i></a>
             </li>
             <li class="nav-item" tootlip="Tienda">
-                <a class="nav-link" href="<?= $php . 'PaginaProductos.php'?>"><i class="nf nf-md-shopping"></i></a>
+                <a class="nav-link" href="<?= $php . 'PaginaProductos.php' ?>"><i class="nf nf-md-shopping"></i></a>
             </li>
             <li class="nav-item" tootlip="Acerca de">
-                <a class="nav-link" href="<?= $php . 'acercaDe.php'?>"><i class="nf nf-fa-info_circle"></i></a>
+                <a class="nav-link" href="<?= $php . 'acercaDe.php' ?>"><i class="nf nf-fa-info_circle"></i></a>
             </li>
             <li class="nav-item" tootlip="Contacto">
                 <a class="nav-link" href="#"><i class="nf nf-fa-phone_square"></i></a>
             </li>
             <li class="nav-item" tootlip="Preguntas frecuentes">
-                <a class="nav-link" href="<?= $php . 'FAQs.php'?>"><i class="nf nf-fa-question_circle"></i></a>
+                <a class="nav-link" href="<?= $php . 'FAQs.php' ?>"><i class="nf nf-fa-question_circle"></i></a>
             </li>
         </ul>
         <div class="navbar-nav ml-auto mr-3">
@@ -41,21 +44,21 @@ $php = $CONFIG['P_php'];
             $nombre = isset($_SESSION['name']) ? $_SESSION['name'] : (isset($_COOKIE['name']) ? $_COOKIE['name'] : '');
             if ($nombre != '') :
             ?>
-            <li class="nav-item" tootlip="Perfil">
-                <a class="nav-link" href="#"><?= $_SESSION['name']?></a>
-            </li>
-            <li class="nav-item" tootlip="Cerrar sesión">
-                <a class="nav-link" id="logOutNav"><i class="nf nf-md-logout"></i></a>
-            </li>
-            <li class="nav-item" tootlip="Carrito">
-                <a class="nav-link" href="#"><i class="nf nf-md-cart_variant"></i></a>
-            </li>
+                <li class="nav-item" tootlip="Perfil">
+                    <a class="nav-link" href="#"><?= $_SESSION['name'] ?></a>
+                </li>
+                <li class="nav-item" tootlip="Cerrar sesión">
+                    <a class="nav-link" id="logOutNav"><i class="nf nf-md-logout"></i></a>
+                </li>
+                <li class="nav-item" tootlip="Carrito">
+                    <a class="nav-link" href="#"><i class="nf nf-md-cart_variant"></i></a>
+                </li>
             <?php
             else :
             ?>
-            <li class="nav-item" tootlip="Iniciar sesión">
-                <a class="nav-link" id="logInNav"><i class="nf nf-md-login"></i></a>
-            </li>
+                <li class="nav-item" tootlip="Iniciar sesión">
+                    <a class="nav-link" id="logInNav"><i class="nf nf-md-login"></i></a>
+                </li>
             <?php
             endif;
             ?>
