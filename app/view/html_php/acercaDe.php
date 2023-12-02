30085186e57737bc024274f10aca6717ecd2b52c
diosescalera <al217108@edu.uaa.mx>
2023-12-02 00:04:37 -0600

    Formato

--- app/view/html_php/acercaDe.php
+++ app/view/html_php/acercaDe.php
@@ -1,70 +1,75 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-    <link rel="stylesheet" href="../css/nosotros.css">
-    <title>Document</title>
-</head>
-<?php include_once("navbar.php")?>
-<body>
-    <div class="Tit">
-    <br><h1 class="animate__animated animate__backInDown">ACERCA DE NOSOTROS</h1><hr>
-    </div>
 
-    <div class="Historia">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+  <!-- Jquery -->
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+  <link rel="stylesheet" href="../css/nosotros.css">
+  <title>Document</title>
+</head>
+<?php include_once("navbar.php") ?>
+
+<body>
+  <div class="Tit">
+    <br>
+    <h1 class="animate__animated animate__backInDown">ACERCA DE NOSOTROS</h1>
+    <hr>
+  </div>
+
+  <div class="Historia">
     <img src="../../media/images/LogoSF.webp" alt="">
     <div>
-        <p>
-            En FluffyHugs, una empresa familiar fundada en 2023, nos dedicamos a enriquecer vidas con nuestros peluches únicos y cariñosamente creados.
-            Nuestro compromiso con la calidad, sostenibilidad y creatividad se refleja en cada diseño, desde tiernos animales hasta imaginativas creaciones. Valoramos la conexión con nuestros clientes, ofreciendo más que juguetes: compañeros de vida que crean recuerdos inolvidables y un servicio excepcional que nos distingue.    
-        </p>
-        <p>
-            Creemos firmemente en la calidad, la sostenibilidad y la creatividad. Por eso, cada uno de nuestros peluches está hecho con los mejores materiales, garantizando no solo suavidad y durabilidad, sino también el respeto por nuestro planeta
-        </p>
-        <p>
-            ¡Únete a nuestra familia y descubre por qué FluffyHugs es más que una tienda de peluches, es un lugar donde los sueños se hacen realidad!
-        </p>
+      <p>
+        En FluffyHugs, una empresa familiar fundada en 2023, nos dedicamos a enriquecer vidas con nuestros peluches únicos y cariñosamente creados.
+        Nuestro compromiso con la calidad, sostenibilidad y creatividad se refleja en cada diseño, desde tiernos animales hasta imaginativas creaciones. Valoramos la conexión con nuestros clientes, ofreciendo más que juguetes: compañeros de vida que crean recuerdos inolvidables y un servicio excepcional que nos distingue.
+      </p>
+      <p>
+        Creemos firmemente en la calidad, la sostenibilidad y la creatividad. Por eso, cada uno de nuestros peluches está hecho con los mejores materiales, garantizando no solo suavidad y durabilidad, sino también el respeto por nuestro planeta
+      </p>
+      <p>
+        ¡Únete a nuestra familia y descubre por qué FluffyHugs es más que una tienda de peluches, es un lugar donde los sueños se hacen realidad!
+      </p>
     </div>
-</div>
-    <p class="eslogan">
-        "Transformando momentos en abrazos suaves y esponjosos"
-    </p><br>
+  </div>
+  <p class="eslogan">
+    "Transformando momentos en abrazos suaves y esponjosos"
+  </p><br>
 
-    <div class="cajitas">
+  <div class="cajitas">
     <div class="card-group">
-  <div class="card">
-    <img class="card-img-top" src="../../media/images/vision.jpeg" alt="Card image cap" width="auto" height="auto">
-    <div class="card-body">
-      <h5 class="card-title">Visión:</h5>
-      <p class="card-text">Nuestra visión es ser líderes en el mundo de los peluches, inspirando sonrisas y creando momentos mágicos para personas de todas las edades. Nos esforzamos por innovar y expandir nuestra gama de productos, manteniendo siempre un compromiso firme con la sostenibilidad y la alegría pura.</p>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="../../media/images/mision.jpeg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Misión:</h5>
-      <p class="card-text">Nuestra misión es enriquecer vidas a través de nuestros peluches, ofreciendo productos de alta calidad, seguros y sostenibles. Nos comprometemos a crear experiencias memorables, apoyando la imaginación y el bienestar emocional de nuestros clientes.</p>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="../../media/images/objetivo.jpeg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Objetivos:</h5>
-      <p class="card-text">Calidad y Seguridad: Garantizar que cada peluche cumpla con los más altos estándares de calidad y seguridad.
+      <div class="card">
+        <img class="card-img-top" src="../../media/images/vision.jpeg" alt="Card image cap" width="auto" height="auto">
+        <div class="card-body">
+          <h5 class="card-title">Visión:</h5>
+          <p class="card-text">Nuestra visión es ser líderes en el mundo de los peluches, inspirando sonrisas y creando momentos mágicos para personas de todas las edades. Nos esforzamos por innovar y expandir nuestra gama de productos, manteniendo siempre un compromiso firme con la sostenibilidad y la alegría pura.</p>
+        </div>
+      </div>
+      <div class="card">
+        <img class="card-img-top" src="../../media/images/mision.jpeg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Misión:</h5>
+          <p class="card-text">Nuestra misión es enriquecer vidas a través de nuestros peluches, ofreciendo productos de alta calidad, seguros y sostenibles. Nos comprometemos a crear experiencias memorables, apoyando la imaginación y el bienestar emocional de nuestros clientes.</p>
+        </div>
+      </div>
+      <div class="card">
+        <img class="card-img-top" src="../../media/images/objetivo.jpeg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Objetivos:</h5>
+          <p class="card-text">Calidad y Seguridad: Garantizar que cada peluche cumpla con los más altos estándares de calidad y seguridad.
             Sostenibilidad: Implementar prácticas ecológicas en la producción y distribución de nuestros productos.
             Innovación: Continuar innovando en diseño y funcionalidad para ofrecer una gama diversa y atractiva de peluches.
             Satisfacción del Cliente: Mantener una excelente atención al cliente y asegurar la plena satisfacción en cada compra.
             Crecimiento: Expandir nuestra presencia en el mercado, tanto a nivel nacional como internacional.</p>
+        </div>
+      </div>
     </div>
-  </div>
-    </div>
-    </div><br>
-    
-<?php include_once("footer.php")?>
+  </div><br>
+
+  <?php include_once("footer.php") ?>
 </body>
+
 </html>
-
-
