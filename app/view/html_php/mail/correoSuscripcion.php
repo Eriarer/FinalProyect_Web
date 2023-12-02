ad503a6040be3b88d0abd3260e2d20c68f6374e5
diosescalera <al217108@edu.uaa.mx>
2023-12-01 20:14:30 -0600

    $recipientMessage Ahora almacena la ruta a un archivo html con el contenido del correo.

--- app/view/html_php/mail/correoSuscripcion.php
+++ app/view/html_php/mail/correoSuscripcion.php
@@ -14,29 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 // Asunto y mensaje
 $recipientSubject = mb_encode_mimeheader('Suscripción a la lista de correo', 'UTF-8', 'Q');
-$recipientMessage = '
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-
-<body>
-    <div>
-        <h1>Gracias por suscribirte a FluffyHugs</h1>
-        <p>Nos emociona tenerte a bordo, pronto recibirás las últimas actualizaciones directamente en tu bandeja de
-            entrada. Queremos asegurarnos de que siempre estés al tanto de los nuevos peluches, promociones
-            exclusivas y eventos emocionantes.</p>
-        <p>No dudes en ponerte en contacto con nosotros si tienes alguna pregunta o sugerencia. </p>
-        <p>Con cariño,</p>
-        <p>El equipo de FluffyHugs.</p>
-    </div>
-</body>
-
-</html>
-';
+$recipientMessage = 'correoSuscripcion.html';
 
 // Incluir el archivo correo.php
 include_once 'correo.php';
