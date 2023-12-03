7da795d7a589a61475fdd6ec1460456c1c10f770
diosescalera <al217108@edu.uaa.mx>
2023-12-02 23:38:12 -0600

    Ruta corregida

--- app/model/mail/suscripcion/correoSuscripcion.php
+++ app/model/mail/suscripcion/correoSuscripcion.php
@@ -1,5 +1,5 @@
 <?php
-include_once __DIR__ . '/../../../routes_files.php';
+include_once __DIR__ . '/../../routes_files.php';
 // Correo y nombre del destinatario
 $recipientEmail = '';
 $recipientName = '';
