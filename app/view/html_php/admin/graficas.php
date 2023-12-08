5f9bb91ade2e57ad838d5491bbc993d2643c82e5
Eri <103050349+Eriarer@users.noreply.github.com>
2023-12-08 16:19:04 -0600

    graficas

--- app/view/html_php/admin/graficas.php
+++ app/view/html_php/admin/graficas.php
@@ -18,12 +18,121 @@ session_start();
   <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
   <!-- agregando link para darle estilos a la alerta -->
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <!-- Chart.js -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.5.1/chart.min.js"></script>
   <!-- CSS -->
   <link rel="stylesheet" href="../../css/main.css" />
 </head>
 
 <body>
+  <?php require_once '../navbar.php'; ?>
+  <div class="row row-cols-1 row-cols-lg-2">
+    <div class="col ">
+      <canvas id="chart1" width="100%" height="100%"></canvas>
+    </div>
+    <div class="col ">
+      <canvas id="chart2" width="100%" height="100%"></canvas>
+    </div>
+  </div>
+  <?php require_once '../footer.php'; ?>
 
+  <script>
+    // Chart 1
+    // grafica de pie
+    let pieChart, barsChart;
+    $(document).ready(function() {
+      initCharts();
+      //sleep 3segundos
+
+    });
+
+    function initCharts() {
+      let canva1, canva2;
+      canva1 = document.getElementById('chart1').getContext('2d');
+      canva2 = document.getElementById('chart2').getContext('2d');
+
+      // Chart 1
+      pieChart = new Chart(canva1, {
+        type: 'pie',
+        data: {
+          labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
+          datasets: [{
+            label: 'Dataset 1',
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: ['#ff0000', '#ff8000', '#ffff00', '#00ff00', '#0000ff']
+          }]
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              position: 'bottom',
+            },
+            title: {
+              display: true,
+              text: 'Lo más vendido'
+            }
+          }
+        },
+      });
+
+      // Chart 2
+      barsChart = new Chart(canva2, {
+        type: 'bar',
+        data: {
+          labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
+          datasets: [{
+              label: 'Dataset 1 (Bars)',
+              data: [1, 10, 1, 4, 1],
+              backgroundColor: ['#0000ff', '#ff0000', '#ff8000', '#ffff00', '#00ff00'],
+              order: 2
+            },
+            {
+              label: 'Dataset 2 (Bars)',
+              data: [11, 9, 2, 1, 1],
+              backgroundColor: ['#ff0000', '#ff8000', '#ffff00', '#00ff00', '#0000ff'],
+              order: 2
+            },
+            {
+              label: 'Total',
+              data: [12, 19, 3, 5, 2],
+              type: 'line',
+              borderColor: '#000000',
+              fill: false,
+              order: 0
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              position: 'bottom',
+              //filtrar la leyendas que el label sea hide y ocultarlas
+              labels: {
+                filter: function(item, chart) {
+                  // Oculta el label solo para el Dataset 3 (Line)
+                  return item.datasetIndex !== 2;
+                }
+              }
+            },
+            title: {
+              display: true,
+              text: 'Ventas'
+            }
+          },
+          scales: {
+            x: {
+              stacked: true,
+            },
+            y: {
+              stacked: true
+            }
+          }
+        },
+      });
+    }
+  </script>
 </body>
 
 </html>
