a21ba02751f0fa00d28902f8f23ba274e87bf511
mariana-avila-rivera <marianaavilarivera@gmail.com>
2023-12-09 16:16:37 -0600

    getCarrito funcional + updateCantidad() y getStock

--- app/model/DB/manejoCarrito.php
+++ app/model/DB/manejoCarrito.php
@@ -9,7 +9,6 @@ $db = new dataBase($credentials, $CONFIG);
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $method = $_POST['method'];
     $usr_id = $db->getID($_SESSION['email']);
-
     if ($method === "delete") {
         $prod_id = $_POST['prod_id'];
         $response = $db->eliminarCarrito($usr_id, $prod_id);
@@ -37,6 +36,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         return;
     } else if ($method === "getCarrito") {
         $response = $db->obtenerCarrito($usr_id);
+        // print_r($response);
+        echo $response;
+        return;
+    } else if ($method === "updateCantidad") {
+        $prod_id = $_POST['prod_id'];
+        $cantidad = $_POST['cantidad'];
+        $response = $db->updateCantidad($cantidad, $prod_id, $usr_id);
+        echo json_encode($response);
+        return;
+    } else if ($method === "getStock") {
+        $prod_id = $_POST['prod_id'];
+        $response = $db->getStock($prod_id);
         echo json_encode($response);
         return;
     }
