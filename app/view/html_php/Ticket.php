412646f8a461418265d9ab9a2dc05d253bb3c8ef
paniniL8 <al291692@edu.uaa.mx>
2023-12-08 22:38:28 -0600

    Co-authored-by: AketzaliCastorena <AketzaliCastorena@users.noreply.github.com> Co-authored-by: Michelle Gómez <Michh13@users.noreply.github.com> Co-authored-by: Gerardo Diosdado <diosescalera@users.noreply.github.com>diseño de ticket

---
+++ app/view/html_php/Ticket.php
@@ -0,0 +1,114 @@
+<?php
+// Datos de la factura
+$folioFactura = "123456";
+$idUsuario = "78910";
+$fechaFactura = date("Y-m-d"); // Fecha actual
+$iva = 0.16; // 16% de IVA
+$gastoEnvio = 50.00; // Costo de envío
+
+// Lista de productos comprados (normalmente sería obtenida de una base de datos)
+$productos = [
+    ['nombre' => 'Producto 1', 'precio' => 200.00, 'cantidad' => 2],
+    ['nombre' => 'Producto 2', 'precio' => 150.00, 'cantidad' => 1],
+    ['nombre' => 'Producto 3', 'precio' => 300.00, 'cantidad' => 1]
+];
+
+// Calcular subtotal, IVA y total
+$subtotal = array_sum(array_map(function ($producto) {
+    return $producto['precio'] * $producto['cantidad'];
+}, $productos));
+
+$ModoPago = array('Tarjeta', 'Efectivo');
+
+$totalIva = $subtotal * $iva;
+$total = $subtotal + $totalIva + $gastoEnvio;
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="../css/TicketEstilos.css">
+    <title>Factura</title>
+</head>
+
+<body>
+
+    <div class="factura-container">
+        <table class="datos">
+            <tr>
+                <td class="linea_abajo" style="vertical-align: middle; text-align: right "><img src="../../media/images/LogoSF.png" alt="Logo" width="60px" height="60px"></td>
+                <td class="linea_abajo" id="titulo_fac">Fluffy Hugs Factura</td>
+                <td class="linea_abajo" style="vertical-align: middle;"><img src="../../media/images/LogoSF.png" alt="Logo" width="60px" height="60px"></td>
+            </tr>
+            <tr>
+                <td class="linea_derecha">Folio Factura</td>
+                <td class="linea_derecha">Fluffy Hugs</td>
+                <td>Nombre del usuario</td>
+            </tr>
+            <tr>
+                <td class="linea_derecha"><?php echo $folioFactura; ?></td>
+                <td class="linea_derecha">pixelwaddle@gmail.com</td>
+                <td>Correo</td>
+            </tr>
+            <tr>
+                <td class="linea_derecha">Fecha</td>
+                <td class="linea_derecha">449-123-4567</td>
+                <td>Numero de Telefono</td>
+            </tr>
+            <tr>
+                <td class="linea_derecha linea_abajo"><?php echo $fechaFactura; ?></td>
+                <td class="linea_derecha linea_abajo">Avenida Universidad 940, Ciudad Universitaria, Universidad Autónoma de Aguascalientes, 20100 Aguascalientes, Ags.</td>
+                <td class="linea_abajo">Direccion</td>
+            </tr>
+        </table>
+
+        
+        <div class="factura-body">
+            <table>
+                <tr>
+                    <th style="text-align: center;">Producto</th>
+                    <th style="text-align: center;">Cantidad</th>
+                    <th style="text-align: center;">Precio Unitario</th>
+                    <th style="text-align: center;">Importe</th>
+                </tr>
+                <?php foreach ($productos as $producto) : ?>
+                    <tr class="productos">
+                        <td><?php echo htmlspecialchars($producto['nombre']); ?></td>
+                        <td class="cantidad"><?php echo $producto['cantidad']; ?></td>
+                        <td class="precioU">$<?php echo number_format($producto['precio'], 2); ?></td>
+                        <td>$<?php echo number_format($producto['precio'] * $producto['cantidad'], 2); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+                
+                <div>
+                    <tr>
+                        <td colspan="2" rowspan="5" class="pago">Modo de Pago</td>
+                    </tr>
+                    <tr>
+                        <td class="factura_totales">Subtotal</td>
+                        <td>$<?php echo number_format($subtotal, 2); ?></td>
+                    </tr>
+                    <tr>
+                        <td class="factura_totales">Gasto de Envío</td>
+                        <td>$<?php echo number_format($gastoEnvio, 2); ?></td>
+                    </tr>
+                    <tr>
+                        <td class="factura_totales">IVA</td>
+                        <td>$<?php echo number_format($totalIva, 2); ?></td>
+                    </tr>
+                    <tr>
+                        <td class="total"><strong>Total</strong></td>
+                        <td class="total" style="text-align: left;"><strong>$<?php echo number_format($total, 2); ?></strong></td>
+                    </tr>
+                </div>
+
+            </table>
+        </div>
+
+
+    </div>
+
+</body>
+
+</html>
