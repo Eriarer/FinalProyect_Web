fc25c7357e34348cedb5df5696b7da4cee73230a
AketzaliCastorena <aketzalicastorena18@gmail.com>
2023-12-09 19:41:23 -0600

    Co-authored-by: paniniL8 <paniniL8@users.noreply.github.com> Co-authored-by: Gerardo Diosdado <diosescalera@users.noreply.github.com> Co-authored-by: Michelle Gómez <Michh13@users.noreply.github.com> se recibieron los datos del formulario

--- app/view/html_php/Ticket.php
+++ app/view/html_php/Ticket.php
@@ -1,5 +1,14 @@
 <?php
-// Datos de la factura
+// Datos del formulario
+// $folioFactura = $_POST['nombre_completo']
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombreUsuario = $_POST['nombre-completo'];
+    $correoUsuario = $_POST['email'];
+    $direccion = $_POST['direccion'] . ', ' . $_POST['codigo-postal'] . ', ' . $_POST['ciudad'] . ', ' . $_POST['pais'];
+    $metodoPago = $_POST['MetodoP'];
+    $telefono = $_POST['telefono'];
+}
+// 
 $folioFactura = "123456";
 $idUsuario = "78910";
 $fechaFactura = date("Y-m-d"); // Fecha actual
