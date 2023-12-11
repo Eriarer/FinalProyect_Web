0a6a356013c54e4ed8f9ee3faf49cd4ae862deaf
Laxlev <leofrivgames1@gmail.com>
2023-12-10 22:48:13 -0600

    Resetear cupones a 0(se puedan volver a usar) Co-authored-by: Mariana Ávila Rivera <mariana-avila-rivera@users.noreply.github.com> Co-authored-by: Eri <Eriarer@users.noreply.github.com>

--- test/pruebaBD.php
+++ test/pruebaBD.php
@@ -2,120 +2,44 @@
 include_once __DIR__ . '/../app/model/DB/dataBaseCredentials.php';
 include_once __DIR__ . '/../app/model/routes_files.php';
 include_once __DIR__ . '/../app/model/DB/controllDB.php';
-// Establecer la conexión a la base de datos
+
+
 $conn = new mysqli($credentials['host'], $credentials['user'], $credentials['pass'], $credentials['db']);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-// Función para generar facturas aleatorias
-function generarFacturasAleatorias($conn, $credentials, $CONFIG) {
-    $usuarios = obtenerUsuariosAleatorios($conn);
-    $productos = obtenerProductosAleatorios($conn);
-
-    for ($i = 0; $i < 100; $i++) {
-        $email = $usuarios[array_rand($usuarios)]['usr_email'];
-        $fecha = generarFechaAleatoria();
-        $iva = rand(12, 26); // IVA aleatorio entre 5% y 20%
-        $gastosEnvio = rand(0, 500); // Gastos de envío aleatorios entre $0 y $50
-        $pais = obtenerPaisAleatorio();
-        $direccion = obtenerDireccionAleatoria();
-        // oxxo o tarjeta (Visa, MasterCard)
-        $metodo_pago = rand(0, 1) ? 'oxxo' : (rand(0, 1) ? 'visa' : 'mastercard');
-
-        $numProductos = rand(1, 5); // Número aleatorio de productos en la factura
-        $productosFactura = obtenerProductosAleatorios($conn, $numProductos);
-
-        $factura = new dataBase($credentials, $CONFIG);
-        $factura->altaFactura($email, $productosFactura, $fecha, $iva, $gastosEnvio, $pais, $direccion, $metodo_pago);
-    }
+$usuario = 6;
+$sql = "UPDATE usuarios SET NEWFLUFFY15 = 0 WHERE usr_id = $usuario";
+echo "<br><br>";
+if ($conn->query($sql) === TRUE) {
+    echo "NEWFLUFFY15 updated successfully";
+} else {
+    echo "Error updating record: " . $conn->error;
 }
-
-// Función para obtener usuarios aleatorios
-function obtenerUsuariosAleatorios($conn) {
-    $usuarios = [];
-    $sql = "SELECT * FROM usuarios";
-    $result = $conn->query($sql);
-
-    while ($row = $result->fetch_assoc()) {
-        $usuarios[] = $row;
-    }
-
-    return $usuarios;
+$sql = "UPDATE usuarios SET FLUFFY10 = 0 WHERE usr_id = $usuario";
+echo "<br><br>";
+if ($conn->query($sql) === TRUE) {
+    echo "FLUFFY10 updated successfully";
+} else {
+    echo "Error updating record: " . $conn->error;
 }
-
-// Función para obtener productos aleatorios
-function obtenerProductosAleatorios($conn, $cantidad = 1) {
-    $productos = [];
-    $resultado = [];
-    $sql = "SELECT * FROM productos ORDER BY RAND() LIMIT $cantidad";
-    $result = $conn->query($sql);
-
-    while ($row = $result->fetch_assoc()) {
-        $resultado[] = $row;
-    }
-    // asegurar que el vector de prodcutos tenga los siguietens cambpos
-    // prod_id
-    // cantidad
-    // precio
-    // descuento
-
-    foreach ($resultado as $key => $value) {
-        $productos[$key]['prod_id'] = $value['prod_id'];
-        $productos[$key]['cantidad'] = rand(1, 5);
-        $productos[$key]['precio'] = $value['prod_precio'];
-        $productos[$key]['descuento'] = rand(0, 50);
-    }
-    return $productos;
+$sql = "UPDATE usuarios SET FLUFFY5 = 0 WHERE usr_id = $usuario";
+echo "<br><br>";
+if ($conn->query($sql) === TRUE) {
+    echo "FLUFFY5 updated successfully";
+} else {
+    echo "Error updating record: " . $conn->error;
 }
-
-
-// Función para obtener un país aleatorio
-function obtenerPaisAleatorio() {
-    $paises = ['USA', 'Canada', 'Mexico', 'Argentina', 'España', 'Francia', 'Italia', 'Alemania', 'China', 'Japón'];
-    return $paises[array_rand($paises)];
-}
-
-// Función para obtener una dirección aleatoria
-function obtenerDireccionAleatoria() {
-    $direcciones = ['Calle 123', 'Avenida Principal', 'Calle de los Sueños', 'Avenida del Sol', 'Callejón Oscuro', 'Boulevard de la Luna'];
-    $num = rand(1, 100);
-    return $direcciones[array_rand($direcciones)] . ", " . $num;
-}
-
-
-// Función para generar una fecha aleatoria en diciembre de 2023
-function generarFechaAleatoria() {
-    $fechaInicio = strtotime('2023-6-01');
-    $fechaFin = strtotime('2023-11-30');
-    $fechaAleatoria = rand($fechaInicio, $fechaFin);
-    // la fecha debe tener formato YYYY-MM-DD, asi que hay que convertirla
-    $fechaFormateada = date('Y-m-d', $fechaAleatoria);
-    //asegurar de que sea una fecha válida
-    if (!strtotime($fechaFormateada)) {
-        echo "Fecha inválida";
-        exit;
-    }
-    return $fechaFormateada;
-}
-
-// a la tabla facturas agregar los atributos de:
-// nombre string(255)
-// correo string(255)
-// telefono string(40)
-$sql = "ALTER TABLE facturas ADD COLUMN nombre VARCHAR(255) AFTER usr_id";
-$conn->query($sql);
-$sql = "ALTER TABLE facturas ADD COLUMN correo VARCHAR(255) AFTER nombre";
-$conn->query($sql);
-$sql = "ALTER TABLE facturas ADD COLUMN telefono VARCHAR(40) AFTER correo";
-$conn->query($sql);
-// acutalizar el defautl del iva a 16
-$sql = "ALTER TABLE facturas MODIFY COLUMN iva INT(3) DEFAULT 16";
-$conn->query($sql);
-
-// Llamada a la función para generar facturas aleatorias
-//generarFacturasAleatorias($conn, $credentials, $CONFIG);
-
-echo "<h1>Facturas generadas</h1>";
-// Cerrar la conexión
 $conn->close();
+return;
+// Establecer la conexión a la base de datos
+$db = new dataBase($credentials, $CONFIG);
+
+echo "<br><br>";
+$result = $db->usarCupon(12, "NEWFLUFFY15");
+echo $result ? "true" : "false";
+
+echo "<br><br>";
+$result = $db->usarCupon(12, "NEWFLUFFY15");
+echo $result ? "true" : "false";
