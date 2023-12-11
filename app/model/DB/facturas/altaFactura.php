80de08c3c3581747992d20ee573c1e75f107a434
Eri <103050349+Eriarer@users.noreply.github.com>
2023-12-11 02:28:24 -0600

    actualizacion de la ata Co-authored-by: Mariana Ávila Rivera <mariana-avila-rivera@users.noreply.github.com> Co-authored-by: Leonardo <Laxlev@users.noreply.github.com>

--- app/model/DB/facturas/altaFactura.php
+++ app/model/DB/facturas/altaFactura.php
@@ -6,6 +6,10 @@ require_once '../../routes_files.php';
 $db = new dataBase($credentials, $CONFIG);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // datos a recibir del POST
+  //$email, $productos, $fecha, $iva, $gastos_envio, 
+  //$pais, $direccion, $metodo_pago, $nombre, $correo, 
+  //$telefono, $subtotal, $total, $cupon, $costo_iva
   $email = $_POST['email'];
   $productosFactura = $_POST['productosFactura'];
   $fecha = date("Y-m-d", strtotime($_POST['fecha']));
@@ -14,8 +18,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $pais = $_POST['pais'];
   $direccion = $_POST['direccion'];
   $metodo_pago = $_POST['metodo_pago'];
+  $nombre = $_POST['nombre'];
+  $correo = $_POST['correo'];
+  $telefono = $_POST['telefono'];
+  $subtotal = $_POST['subtotal'];
+  $total = $_POST['total'];
+  $cupon = $_POST['cupon'];
+  $costo_iva = $_POST['costo_iva'];
 
-  $result = $db->altaFactura($email, $productosFactura, $fecha, $iva, $gastosEnvio, $pais, $direccion, $metodo_pago);
+  $result = $db->altaFactura($email, $productosFactura, $fecha, $iva, $gastosEnvio, $pais, $direccion, $metodo_pago, $nombre, $telefono, $subtotal, $total, $cupon, $costo_iva);
   echo $result;
   return;
 }
